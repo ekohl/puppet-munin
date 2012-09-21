b80a07c115e30840907bd5a789fb35ea4f388a1f
Ewoud Kohl van Wijngaarden <e.kohlvanwijngaarden@oxilion.nl>
2012-09-21 16:33:48 +0200

    Use the built in regsubst instead of gsub

--- manifests/plugins/interfaces.pp
+++ manifests/plugins/interfaces.pp
@@ -1,8 +1,8 @@
 # handle if_ and if_err_ plugins
 class munin::plugins::interfaces inherits munin::plugins::base {
 
-	$ifs = gsub(split($munin_interfaces, " "), "(.+)", "if_\\1")
-	$if_errs = gsub(split($munin_interfaces, " "), "(.+)", "if_err_\\1")
+	$ifs = regsubst(split($munin_interfaces, " "), "(.+)", "if_\\1")
+	$if_errs = regsubst(split($munin_interfaces, " "), "(.+)", "if_err_\\1")
 	plugin {
 		$ifs: ensure => "if_";
 		$if_errs: ensure => "if_err_";
